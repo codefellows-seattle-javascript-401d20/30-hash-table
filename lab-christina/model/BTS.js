8a11a2f89d54cbe2143de8691923bf711c5d3f93
Christina <penssake@gmail.com>
2017-10-24 01:35:14 -0700

    working with BST

--- lab-christina/model/BTS.js
+++ lab-christina/model/BTS.js
@@ -1,40 +1,26 @@
 'use strict';
 
-class Node {
+class BinarySearchTree {
   constructor(value){
     this.value = value;
     this.next = null;
     this.previous = null;
   }
 
-}
-class BinarySearchTree {
-  constructor(){
-    this.root = null;
-  }
-
   insert(value) {
-    let node = this.root;
-    if(this.root === null){
-      this.root = new Node(value);
-    } else {
-      let createTree = function() {
-        while(this.value !== node.value)
-          if(this.value < node.value && node.previous === null)
-            node.previous = this;
-        if(this.value < node.value && node.previous !== null)
-          return createTree(node.previous);
-        if (this.value > node.value && node.next === null)
-          node.next = this;
-        if(this.value > node.value && node.next !== null)
-          return createTree(node.next);
-      };
-    }
+    let currentNode = new BinarySearchTree(value);
+    let createTree = function() {
+      while(this.value !== currentNode.value)
+        if(this.value < currentNode.value && currentNode.previous === null)
+          currentNode.previous = this;
+      if(this.value < currentNode.value && currentNode.previous !== null)
+        return createTree(currentNode.previous);
+      if (this.value > currentNode.value && currentNode.next === null)
+        currentNode.next = this;
+      if(this.value > currentNode.value && currentNode.next !== null)
+        return createTree(currentNode.next);
+    };
   }
 }
-
-new Node(4);
-new Node(5);
-
 
 module.exports = BinarySearchTree;
