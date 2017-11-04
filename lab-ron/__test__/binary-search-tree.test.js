02fc01d4c4cb4182e1607c9891b0028e761024a2
Ron Barrantes <ronbarrantes@gmail.com>
2017-11-03 23:27:46 -0700

    bst adding

--- lab-ron/__test__/binary-search-tree.test.js
+++ lab-ron/__test__/binary-search-tree.test.js
@@ -3,15 +3,19 @@ const BST = require('../model/binary-search-tree.js')
 describe('Binary Search Trees', () => {
 
   test('creating a new bst', () => {
-    const tree = new BST()
+    const tree = new BST(3)
     expect(tree).toBeInstanceOf(BST)
   })
 
-  // test('inserting a value', () => {
-  //   const tree = new BST()
+  test('inserting a value', () => {
+    const tree = new BST(3)
+    tree.insert(5)
+    // .insert(3)
+    // .insert(2)
 
-  //   tree.insert(3)
-  //   expect(tree).toBeInstanceOf(BST)
-  //   expect(tree.value).toEqual(3)
-  // })
+
+    console.log('VARIABLES:', tree.left, tree.right, tree.value)
+    console.log('TYPEOF', typeof tree)
+    expect(tree).toBeInstanceOf(BST)
+  })
 })
--- lab-ron/model/binary-search-tree.js
+++ lab-ron/model/binary-search-tree.js
@@ -4,14 +4,60 @@ module.exports = class BinarySearchTree {
     this.left = null
     this.right = null
   }
-
   // Add an insert method that appends a node to the BST
 
 
-  insert(node) {
-    if (!(node instanceof BinarySearchTree)) {
-      throw new Error('BINARY_TREE_ERROR: this is not a Binary Search Tree')
+  insert(value) {
+    // if (!(node instanceof BinarySearchTree)) {
+    //   throw new Error('--> BINARY_TREE_ERROR: this is not a Binary Search Tree')
+    // }
+
+    if (!this.value) {
+      this.value = value
     }
+
+    // if (this.value > value) {
+    //   console.log('---> LEFT', value)
+    //   this.left = bst
+    //   return bst.insert(value)
+    // }
+
+
+    //   if (!(node instanceof DoublyLinkedList))
+    //     throw new Error('__USAGE_ERROR__: Node must be a DoublyLinkedList');
+    //   if (!this.next) {
+    //     node.prev = this;
+    //     this.next = node;
+    //   }
+    //   else
+    //     this.next.append(node);
+    //   return this;
+    // }
+
+    // if (this.value < value) {
+    //   console.log('---> RIGHT', value)
+    //   this.right = bst
+    //   return bst.insert(value)
+    // }
+    // if (this.value < value) {
+    //   console.log('---> INSIDE RIGHT', value)
+    //   this.right = bst
+    //   this.right = bst.value
+    // }
+
+    // if (this.value === value) {
+
+    //   return
+    // }
+
+
+
+
+
+
+
+
+    return this
   }
 }
 
